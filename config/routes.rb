c93f74b1cde4a6db61c18fd88e7e5aafc780e2cd
laransom <laransom@gmail.com>
2014-05-18 13:52:50 -0400

    added post controller and set basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Blogger::Application.routes.draw do
+
+  root 'posts#index'
+
+  resources :posts, only: [:index, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
