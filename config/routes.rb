dc07edccf963d270515dc1f7706fed703c7ea74c
laransom <laransom@gmail.com>
2014-05-19 12:23:44 -0400

    added post show page and put posts on index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blogger::Application.routes.draw do
 
   root 'posts#index'
 
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :show, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
